a28eb067910db45775a2b9d7942a6442793b709a
jaegerthomas <bjaegerthomas@gmail.com>
2025-02-27 10:07:18 -0500

    add global types definition to server and updated ts config for server and client

---
+++ server/src/types/global.d.ts
@@ -0,0 +1,23 @@
+declare namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        username: string;
+        email: string;
+      };
+    }
+  }
+  
+  declare module 'react-dom/client' {
+    import { ReactElement } from 'react';
+  
+    interface Root {
+      render(children: ReactElement): void;
+      unmount(): void;
+    }
+  
+    function createRoot(container: Element | DocumentFragment): Root;
+  
+    export { createRoot };
+  }
+  
